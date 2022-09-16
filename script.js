780c003881723a419d3e3df150ccb3cd12fa152e
knika-starkey <knika787@gmail.com>
2022-09-16 22:45:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let n = prompt("Enter number");
+let x = 1;
+let sum = 0;
+if (!isNaN(n)) {
+  for (let i = 0; i < n; i++) {
+    x *= 2;
+    sum += x;
+    //document.write(x + " ");
+  }
+  document.write(
+    `Now ypu hawe ${sum} grains of wheat that weigh ${
+      (sum * 0.065) / 1000
+    } kilogram`
+  );
+} else alert("ERROR");
+
+// let a, b;
+// a = 1;
+// b = 1;
+// a = ++b;
+// console.log(a);
